71e3539f58e61618ddd1e5fe07ddeca2b12332d0
Meike van Leerdam <mbv32@cornell.edu>
2026-08-31 14:54:58 -0400

    changed description

--- files/assignment_week_2.py
+++ files/assignment_week_2.py
@@ -34,9 +34,9 @@ def convert_milk_yield(value):
 
 #Step 8: Try to merge the "milk_yield_conversion" branch into the main branch. 
 
-#Step 9: Resolve any merge conflicts that arise during the merge process by renaming the function names so they better reflect what they do.
+#Step 9: Resolve any merge conflicts that arise during the merge process by keeping only the changes from main. 
 
-#Step 10: Delete the "milk_yield_conversion" branch as it is no longer needed after the merge is complete.
+#Step 10: Go back to the milk_conversion_branch and change the name of the function to "convert_milk_yield" and update the docstring accordingly.
 
 #Step 11: Push the changes to the remote repository and verify that the merge was successful by checking the main branch for both functions.
 
